bfbff6e067aeb98e6a4ceeedc2482d86c7bdbb00
Teguhriyadi <ilham.teguh55@gmail.com>
2021-05-12 16:58:35 +0700

    update sql terbaru

--- db_proyek_1.sql
+++ db_proyek_1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Bulan Mei 2021 pada 12.05
--- Versi server: 10.1.38-MariaDB
--- Versi PHP: 7.3.2
+-- Generation Time: May 12, 2021 at 11:57 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
 CREATE TABLE `barang` (
@@ -38,43 +38,42 @@ CREATE TABLE `barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang`
+-- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`kode_barang`, `id_kategori`, `nama_barang`, `harga`, `satuan`, `keterangan`) VALUES
-('BR-003', 4, 'Terigu 1/2', 1500, 'setengah', 'e'),
+('BR-003', 11, 'Terigu 1/25', 20000, 'kg', 'Data Barang Terigu 1/2'),
 ('BR-004', 1, 'Beras', 10000, 'kg', 'Ini adalah data beras'),
 ('BR-005', 1, 'Beras', 10000, 'kg', '1'),
-('BR-006', 4, 'Bon Cabe', 1000, 'pcs', 'Ini Adalah data bon cabe'),
 ('BR-007', 1, 'Kecap Bango', 5000, 'pcs', 'Data Kecap Bango');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `informasi`
+-- Table structure for table `informasi`
 --
 
 CREATE TABLE `informasi` (
   `id_informasi` int(11) NOT NULL,
   `nama_informasi` varchar(100) NOT NULL,
   `keterangan` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `status` int(11) DEFAULT '0',
   `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `informasi`
+-- Dumping data for table `informasi`
 --
 
 INSERT INTO `informasi` (`id_informasi`, `nama_informasi`, `keterangan`, `created_at`, `status`, `id_user`) VALUES
-(1, 'Promo', 'Milku Promo 2 Gratis 1 wrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrw', '2021-05-09 00:59:08', 0, 1),
-(2, 'Promo', 'Minyak Bimoli Promo', '2021-05-09 00:28:04', 0, 2);
+(1, 'Promo', 'Milku Promo 2 Gratis 1 wrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrwrw', '2021-05-09 00:59:08', 1, 1),
+(2, 'Promo', 'Minyak Bimoli Promo', '2021-05-09 00:28:04', 1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kategori`
+-- Table structure for table `kategori`
 --
 
 CREATE TABLE `kategori` (
@@ -83,22 +82,22 @@ CREATE TABLE `kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kategori`
+-- Dumping data for table `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
-(1, 'Sembako'),
 (3, 'Minuman'),
 (4, 'Makanan 123'),
 (5, 'Mohammad Ilham'),
-(6, 'Bayar PDH'),
-(10, 'Buah - Buahan 123'),
-(11, 'Mohammad Ilham');
+(6, 'Mohammad Ilham 123'),
+(11, 'Mohammad Ilham'),
+(12, '1'),
+(13, '232');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pelanggan`
+-- Table structure for table `pelanggan`
 --
 
 CREATE TABLE `pelanggan` (
@@ -110,7 +109,7 @@ CREATE TABLE `pelanggan` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `supplier`
+-- Table structure for table `supplier`
 --
 
 CREATE TABLE `supplier` (
@@ -122,19 +121,17 @@ CREATE TABLE `supplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `supplier`
+-- Dumping data for table `supplier`
 --
 
 INSERT INTO `supplier` (`kode_supplier`, `nama_supplier`, `no_telepon`, `keterangan`, `status`) VALUES
-('KS-003', 'M Ilham  ', '76543113131', 'Supplier Minuman Mizone', 1),
-('SU-006', 'Sahrul Fazri Udin', '8582932893', 'Data Supplier Barang', 1),
-('SU-007', 'Ahmad Hidayat Hendrayawan 123', '1234567', 'Ini adalah Data Ahmad Hidayat', 1),
-('SU-008', 'Ahmad Budiman', '12345', 'Data Supplier Ahmad Budiman', 1);
+('12345', 'Ahmad Fauzi', '122345', 'Pocari Sweat', 1),
+('S002346', 'Mohammad Ilham Teguhriyadi', '12345', 'Data Supplier Mizone', 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi_barang`
+-- Table structure for table `transaksi_barang`
 --
 
 CREATE TABLE `transaksi_barang` (
@@ -142,28 +139,28 @@ CREATE TABLE `transaksi_barang` (
   `kode_barang` varchar(100) DEFAULT NULL,
   `stok` int(100) DEFAULT NULL,
   `tanggal` datetime DEFAULT NULL,
-  `status` int(1) DEFAULT NULL
+  `status` int(1) DEFAULT NULL,
+  `kode_supplier` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `transaksi_barang`
+-- Dumping data for table `transaksi_barang`
 --
 
-INSERT INTO `transaksi_barang` (`id_transaksi`, `kode_barang`, `stok`, `tanggal`, `status`) VALUES
-(6, 'BR-003', 3, '2021-05-09 06:09:39', 1),
-(7, 'BR-004', 1, '2021-05-06 23:28:40', 1),
-(8, 'BR-003', 1, '2021-05-06 23:37:30', 0),
-(9, 'BR-003', 1, '2021-05-06 23:40:50', 1),
-(10, 'BR-005', 2, '2021-05-06 23:47:58', 1),
-(12, 'BR-004', 4, '2021-05-09 05:25:35', 1),
-(13, 'BR-005', 1, '2021-05-09 05:37:26', 0),
-(14, 'BR-005', 1, '2021-05-09 05:37:33', 0),
-(15, 'BR-005', 1, '2021-05-09 06:32:34', 1);
+INSERT INTO `transaksi_barang` (`id_transaksi`, `kode_barang`, `stok`, `tanggal`, `status`, `kode_supplier`) VALUES
+(2, 'BR-003', 5, '2021-05-12 14:18:45', 1, '12345'),
+(3, 'BR-004', 2, '2021-05-12 14:18:54', 1, 'NULL'),
+(4, 'BR-004', 1, '2021-05-12 14:26:02', 0, 'NULL'),
+(5, 'BR-004', 1, '2021-05-12 14:29:46', 0, 'NULL'),
+(6, 'BR-005', 5, '2021-05-12 14:31:45', 1, ''),
+(7, 'BR-003', 1, '2021-05-12 16:50:56', 0, 'NULL'),
+(8, 'BR-003', 1, '2021-05-12 16:51:18', 1, ''),
+(9, 'BR-004', 3, '2021-05-12 16:52:22', 1, 'NULL');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -171,94 +168,94 @@ CREATE TABLE `users` (
   `username` varchar(191) DEFAULT NULL,
   `email` varchar(191) DEFAULT NULL,
   `password` varchar(191) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `last_login` datetime DEFAULT NULL,
   `level` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `updated_at`, `last_login`, `level`) VALUES
-(1, 'ilham', 'ilham@gmail.com', 'ilham', '2021-05-10 09:56:44', '2021-04-29 09:44:03', '2021-05-10 16:56:44', 'admin'),
-(2, 'admin', 'ilham.teguh55@gmail.com', '', '2021-05-06 15:39:13', '2021-05-06 10:39:13', '2021-05-06 17:39:13', 'admin'),
-(3, 'farhan', 'farhan@gmail.com', 'farhan', '2021-05-09 01:07:11', '2021-05-06 19:22:58', '2021-05-09 08:07:11', 'admin');
+(1, 'asep', 'asep@gmail.com', '$2y$10$DpnuIU40//TNnyK/6mgitO.G9zuHlF1Px51xqhbmOG4BDbrMa2Ab2', '2021-05-10 13:51:04', '2021-05-12 01:58:20', '2021-05-12 16:44:11', 'admin'),
+(2, 'budiman', 'budiman@gmail.com', '$2y$10$IxxQUMamBtqYcjwIC940R.2hJeUVKFUYv00ahDTZJnJJSIFqV/Mf2', '2021-05-06 15:39:13', '2021-05-12 01:57:15', '2021-05-06 17:39:13', 'admin'),
+(11, 'kasir', 'kasir@gmail.com', '$2y$10$t.0lzV8R/i9KVjjyeYatS.pKCY5pjSe3mAW0Y.lNq3iKbCqR3BddS', '2021-05-12 02:47:00', '2021-05-12 02:47:00', '2021-05-12 09:47:52', 'kasir');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `barang`
+-- Indexes for table `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`kode_barang`);
 
 --
--- Indeks untuk tabel `informasi`
+-- Indexes for table `informasi`
 --
 ALTER TABLE `informasi`
   ADD PRIMARY KEY (`id_informasi`);
 
 --
--- Indeks untuk tabel `kategori`
+-- Indexes for table `kategori`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indeks untuk tabel `pelanggan`
+-- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`kode_pelanggan`);
 
 --
--- Indeks untuk tabel `supplier`
+-- Indexes for table `supplier`
 --
 ALTER TABLE `supplier`
   ADD PRIMARY KEY (`kode_supplier`);
 
 --
--- Indeks untuk tabel `transaksi_barang`
+-- Indexes for table `transaksi_barang`
 --
 ALTER TABLE `transaksi_barang`
   ADD PRIMARY KEY (`id_transaksi`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `informasi`
+-- AUTO_INCREMENT for table `informasi`
 --
 ALTER TABLE `informasi`
   MODIFY `id_informasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `kategori`
+-- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT untuk tabel `transaksi_barang`
+-- AUTO_INCREMENT for table `transaksi_barang`
 --
 ALTER TABLE `transaksi_barang`
-  MODIFY `id_transaksi` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_transaksi` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
